1328acf3b5dc3b5f93bba3a099507ba77427fcbb
n.ivanov <giperdrakon@gmail.com>
2024-07-15 11:26:31 +0300

    Update hw5

--- hw5/HW5.c
+++ hw5/HW5.c
@@ -9,49 +9,76 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Node {
+    int value;
+    struct Node* next;
+};
+
+struct Node* add_element(struct Node* head, int value) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    if (!new_node) {
+        abort();
+    }
+    new_node->value = value;
+    new_node->next = head;
+    return new_node;
+}
+
+
 int data[6] = {4, 8, 15, 16, 23, 42};
 
-void transfer_array(int* data_in, int* data_out, int size)
-{
-    int len = size;
-    while (len--)
-    {
-        data_out[len-1] = data_in[len-1];
+struct Node* transfer_array(int* data, int size) {
+    struct Node* head = NULL;
+    for (int i = size - 1; i >= 0; --i) {
+        head = add_element(head, data[i]);
     }
-    
+    return head;
 }
 
-void print_common(int* data_in, int size)
-{
-    for(int i = 0; i < size; i++)
-        printf("%i ", data_in[i]);
+
+void print_common(struct Node* head) {
+    struct Node* current = head;
+    while (current) {
+        printf("%d ", current->value);
+        current = current->next;
+    }
     printf("\r\n");
 }
 
-void print_filter(int* data_in, int size)
-{
-    while (size--)
-        if (data_in[size - 1] & 1)
-            printf("%i ", data_in[size - 1]);
+void print_filter(struct Node* head) {
+    struct Node* current = head;
+    while (current) {
+        if (current->value & 1) {
+            printf("%d ", current->value);
+        }
+        current = current->next;
+    }
     printf("\r\n");
+}
+
+void free_list(struct Node* head) {
+    struct Node* current = head;
+    struct Node* next;
+    while (current) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 1) {
-        printf("Использование: %s>\n", argv[0]);
+        printf("Использование: %s\n", argv[0]);
         return 1;
     }
 
-    int* data_tmp;
-    data_tmp = malloc(sizeof(data));
-    
-    int len = sizeof(data) / sizeof(data[0]);
+    struct Node* data_list = transfer_array(data, sizeof(data) / sizeof(data[0]));
 
-    transfer_array(data, data_tmp, len);
+    print_common(data_list);
 
-    print_common(data_tmp, len);
+    print_filter(data_list);
 
-    print_filter(data_tmp, len);
+    free_list(data_list);
 
     return 0;
 }
